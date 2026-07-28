01447b442623d7a9d44d06670bc2fcb9cc97e8dd
Yash Lohar <yashhhh9005@gmail.com>
2026-07-28 20:10:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (33.45%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0)
+        return 0;
+
+        if (n == 1)
+        return 1;
+
+        int first = 0;
+        int second = 1;
+
+        for(int i= 2; i <= n; i++){
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        return second;
+    }
+}
